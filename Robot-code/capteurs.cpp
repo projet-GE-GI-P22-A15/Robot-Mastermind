a525b4d3922d5182fe5057dcd9bd63299d407d12
justinbolduc <justinbolduc1@hotmail.com>
2015-10-18 15:55:19 -0400

    Capteurcouleur

--- Robot-code/capteurs.cpp
+++ Robot-code/capteurs.cpp
@@ -2,3 +2,14 @@
 
 //code capteur ici!
 
+
+*/
+/**
+ * Détection d'obstacles avec le sonar:
+ */
+// float SONAR_Detect(int sonar_number)
+
+/**
+ * Detection infra rouge donc mur ou obstacle
+ */
+ // IR_Detect()
